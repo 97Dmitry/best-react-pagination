2e14db8a68b98f1f2414b5237233a5037abdd3b0
97Dmitry <dart_veyder_1997@mail.ru>
2021-12-18 20:00:41 +0600

    feat: restructuring has been carried and improve building process

---
+++ dist/components/Wrapper/Wrapper.d.ts
@@ -0,0 +1,7 @@
+import { FC } from "react";
+interface WrapperProps {
+    isDefaultStyles: boolean | undefined;
+    wrapperClass: string;
+}
+declare const Wrapper: FC<WrapperProps>;
+export default Wrapper;
